16e68e30b412864554cc07094e3a4aaff3d59cca
Abhinav Singh <abhinavsingh6633@gmail.com>
2025-05-24 19:16:47 +0530

    stable build and ui updates

---
+++ src/components/LabeledInputs.tsx
@@ -0,0 +1,32 @@
+import { Dispatch, SetStateAction } from "react";
+import { Text, TextInput, View } from "react-native";
+
+type props = {
+  value: string;
+  placeholder?: string;
+  setValue: Dispatch<SetStateAction<string>>;
+  title?: string;
+};
+
+export default function TextInputs({
+  value,
+  placeholder,
+  setValue,
+  title,
+}: props) {
+  return (
+    <View>
+      {title ? (
+        <Text className="mb-2 text-gray-500 font-semibold">{title}</Text>
+      ) : (
+        ""
+      )}
+      <TextInput
+        value={value}
+        placeholder={placeholder}
+        onChangeText={setValue}
+        className="border border-gray-500 p-3 rounded-md"
+      />
+    </View>
+  );
+}
