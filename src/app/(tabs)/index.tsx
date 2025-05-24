e4efb812ab8e87943e7003700d753db686a60973
Abhinav Singh <abhinavsingh6633@gmail.com>
2025-05-24 19:28:38 +0530

    header styles updated and status bar added (#1)

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,19 +1,28 @@
-import { FlatList } from "react-native";
+import { FlatList, StatusBar, View } from "react-native";
 import posts from "~/assets/data/post.json";
 import PostListItem from "~/src/components/PostListItem";
 
 export default function FeedScreen() {
   return (
-    <FlatList
-      data={posts}
-      renderItem={({ item }) => <PostListItem post={item} />}
-      contentContainerStyle={{
-        gap: 10,
-        width: "100%",
-        maxWidth: 512,
-        alignSelf: "center",
-      }}
-      showsVerticalScrollIndicator={false}
-    />
+    <View>
+      <StatusBar
+        animated={true}
+        backgroundColor="#ffffff"
+        barStyle="dark-content"
+        hidden={false}
+      />
+
+      <FlatList
+        data={posts}
+        renderItem={({ item }) => <PostListItem post={item} />}
+        contentContainerStyle={{
+          gap: 10,
+          width: "100%",
+          maxWidth: 512,
+          alignSelf: "center",
+        }}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
   );
 }
